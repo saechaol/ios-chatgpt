5ad30480d82a811207917878744276bfd49db982
saechaol <33850575+saechaol@users.noreply.github.com>
2025-06-26 19:19:07 -0700

    initial commit

---
+++ iOS-GPT/iOS-GPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-GPT
+//
+//  Created by 세차오 루카스 on 6/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-GPT/iOS-GPT/iOS_GPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_GPTApp.swift
+//  iOS-GPT
+//
+//  Created by 세차오 루카스 on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_GPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
